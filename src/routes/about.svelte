3ee83e80e1b4a80f9ce01ab1e6523ee05b6176cc
nathanaelsanilo <nathanaelsanilo@gmail.com>
2021-08-14 11:25:55 +0700

    syntax format

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-    <title>About the Svelte Kit Pokedex</title>
+	<title>About the Svelte Kit Pokedex</title>
 </svelte:head>
 <h1 class="text-4xl text-center my-8 uppercase">about</h1>
 <p>this is a really cool svelte demo that uses the pokemon dex API</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,47 +1,57 @@
 <script context="module">
-    export async function load({ page }) {
-        const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
-        const res = await fetch(url);
-        const data = await res.json();
-        const loadedPokemon = data.results.map((data, index) => {
-            return {
-                name: data.name,
-                id: index + 1,
-                image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
-            };
-        });
-        return { props: { pokemon: loadedPokemon } };
-    }
+	export async function load({ page }) {
+		const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+		const res = await fetch(url);
+		const data = await res.json();
+		const loadedPokemon = data.results.map((data, index) => {
+			return {
+				name: data.name,
+				id: index + 1,
+				image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+					index + 1
+				}.png`
+			};
+		});
+		return { props: { pokemon: loadedPokemon } };
+	}
 </script>
+
 <script>
-    import PokemanCard from "../components/pokemanCard.svelte";
-    export let pokemon;
+	import PokemanCard from '../components/pokemanCard.svelte';
+	export let pokemon;
 
-    let searchTerm = "";
-    let filteredPokemon = [];
+	let searchTerm = '';
+	let filteredPokemon = [];
 
-    $: {
-        console.log(filteredPokemon);
-        if (searchTerm) {
-            filteredPokemon = pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
-        } else {
-            filteredPokemon = [ ... pokemon];
-        }
-    }
+	$: {
+		console.log(filteredPokemon);
+		if (searchTerm) {
+			filteredPokemon = pokemon.filter((pokeman) =>
+				pokeman.name.toLowerCase().includes(searchTerm.toLowerCase())
+			);
+		} else {
+			filteredPokemon = [...pokemon];
+		}
+	}
 </script>
 
 <svelte:head>
-    <title>Svelte Pokedex</title>
+	<title>Svelte Pokedex</title>
 </svelte:head>
 
-<h1 class="text-4xl text-center my-8 uppercase">
-    Svelte kit pokedex
-</h1>
+<h1 class="text-4xl text-center my-8 uppercase">Svelte kit pokedex</h1>
 
-<input type="text" name="" id="" placeholder="Search Pokemon" class="w-full rounded-md text-lg p-4 border-2 border-gray-200" bind:value={searchTerm}>
+<input
+	type="text"
+	name=""
+	id=""
+	placeholder="Search Pokemon"
+	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+	bind:value={searchTerm}
+/>
 
 <div class="grid gap-4 md:grid-cols-2 grid-cols-1 py-4">
-    {#each filteredPokemon as pokeman}
-        <PokemanCard pokeman={pokeman} />
-    {/each}
+	{#each filteredPokemon as pokeman}
+		<PokemanCard {pokeman} />
+	{/each}
 </div>
